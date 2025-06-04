99f96659b48e0185cadea06c029b57a777a0b986
Oscar Hernández <ohdz202@gmail.com>
2025-06-04 17:58:02 -0600

    Corrigiendo error de pathLogo en FormLayaout

--- src/Components/FormLayaut.tsx
+++ src/Components/FormLayaut.tsx
@@ -9,7 +9,7 @@ const FormLayaut = () => {
       <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
         <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#1f2a484c,transparent)]"></div>
       </div>
-      <Header styles="bg-header-bg" navLinks={navLinks}>
+      <Header styles="bg-header-bg" navLinks={navLinks} pathLogo="/">
         <div>
           <MenuIcon />
           <div className="hidden md:flex md:items-center md:gap-3 ">
