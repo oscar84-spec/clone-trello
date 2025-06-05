7b2670147a1bef11333a46c8fb8221977511ade3
Oscar Hernández <ohdz202@gmail.com>
2025-06-05 12:33:14 -0600

    Agregando url de la api

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api-trello-clone.onrender.com/",
+  baseURL: "https://api-trello-clone-production.up.railway.app/",
   headers: { "Content-Type": "application/json" },
 });
 
